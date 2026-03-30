af2c8125d13f3746d6c006b746f138db38d8118f
unknown <moath10145.mm@gmail.com>
2026-03-30 11:32:18 +0400

    Third

--- test1.py
+++ test1.py
@@ -1,18 +1,7 @@
-print()
-name = ["123" , "555" , "666"]
-print(int)
-print("--------------------")
-print(f"1002164737")
-print() 
-element = name[0]
+try:
+    user_input = input("Please Enter a number")
+    number = int(user_input)
+    print(f"you entered the number: {number} ")
 
-
-First_Value = float(input("Enter the First Value = " ))
-
-
-Second_Value = float(input("Enter the Second Value = " ))
-
-
-Total = First_Value + Second_Value
-
-print (int(Total))
+except:
+    print("Invalid input, Please Try Again ")
