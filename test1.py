b029ba0d3688f0036ba0709c15a16f06ea8d52b6
unknown <moath10145.mm@gmail.com>
2026-03-30 09:16:16 +0400

    first commit

---
+++ test1.py
@@ -0,0 +1,18 @@
+print()
+name = ["123" , "555" , "666"]
+print(int)
+print("--------------------")
+print(f"1002164737")
+print() 
+element = name[0]
+
+
+First_Value = float(input("Enter the First Value = " ))
+
+
+Second_Value = float(input("Enter the Second Value = " ))
+
+
+Total = First_Value + Second_Value
+
+print (int(Total))
